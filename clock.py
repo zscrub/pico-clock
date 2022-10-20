669fdef99756e6fe5bc6dff43f3a435105065f68
Zack <rubinzack40@gmail.com>
2022-10-20 18:48:34 -0400

    implemented unit attr assignment

--- clock.py
+++ clock.py
@@ -3,30 +3,13 @@ import micropython
 
 from lcd_init import lcd
 
-months = {
-        1: "Jan",
-        2: "Feb",
-        3: "Mar",
-        4: "Apr",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "Aug",
-        9: "Sep",
-        10: "Oct",
-        11: "Nov",
-        12: "Dec"
-}
+from schema import (
+    units,
+    months,
+    weekdays,
+    Units
+)
 
-weekdays = {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday"
-}
 
 def convert_hour(hour: int) -> tuple:
     """Takes integer as parameter to convert to 12 hour clock with time of day."""
@@ -40,30 +23,38 @@ def convert_time(time_values: dict) -> None:
     # need an algorithm to check if any elems are less than 10 and return their index
     # month, mday, hour, minute, second, day
     for k,v in time_values.items():
-        if isinstance(v, int) and v < 10:
-            time_values[k] = f"0{v}"
+        if isinstance(v, int):
+            if v < 10:
+                time_values[k] = f"0{v}"
+            else:
+                time_values[k] = str(v)
 
 now_time = ()
-
 time_values = {}
 
 while True:
     now = time.localtime()
     if now == now_time:
         continue
-
+    
     now_time = now
-    year, month, time_values["mday"], time_values["hour"], time_values["minute"], time_values["second"], day, yearday = now
+    year, time_values["month"], time_values["mday"], time_values["hour"], time_values["minute"], time_values["second"], day, yearday = now
     time_values["hour"], time_of_day = convert_hour(time_values["hour"])
     convert_time(time_values)
-    
-    # output = f"{weekdays[day]}, {months[month]} {time_values['mday']} {year} {time_values['hour']}:{time_values['minute']}:{time_values['second']} {time_of_day}"
-    top_output = f"{weekdays[day]}, {months[month]} {time_values['mday']}"
-    bot_output = f"{year} {time_values['hour']}:{time_values['minute']}:{time_values['second']} {time_of_day}"
-    lcd.clear()
-    # lcd.putstr(output)
-    lcd.putstr(top_output)
-    lcd.move_to(0, lcd.cursor_y+1)
-    lcd.putstr(bot_output)
-    # time.sleep_ms(650)
 
+    Units.year =   year
+    Units.month =  time_values["month"]
+    Units.mday =   time_values["mday"] 
+    Units.day =    weekdays[day]
+    Units.hour =   time_values["hour"]
+    Units.minute = time_values["min"]
+    Units.second = time_values["sec"]
+    Units.tod =    time_of_day
+
+    # output = f"{weekdays[day]}, {*months[month]} {time_values['mday']} {year} {time_values['hour']}:{time_values['minute']}:{time_values['second']} {time_of_day}"
+    top_output = f"{weekdays[day]} {time_values['month']}-{time_values['mday']}-{year}"
+    bot_output = f"{time_values['hour']}:{time_values['minute']}:{time_values['second']}{time_of_day}"
+    lcd.clear()
+
+    lcd.replace_str(1, 0, top_output)
+    lcd.replace_str(3, 1, bot_output)
