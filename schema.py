a69218470865ea760bd77d0a85b4a4abd8a66eb8
Zack <rubinzack40@gmail.com>
2022-10-20 18:49:01 -0400

    moved schema code

---
+++ schema.py
@@ -0,0 +1,46 @@
+
+months = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+}
+
+weekdays = {
+        0: "Mon",
+        1: "Tue",
+        2: "Wed",
+        3: "Thu",
+        4: "Fri",
+        5: "Sat",
+        6: "Sun"
+}
+
+units = {
+    "day": str,
+    "month": str,
+    "mday": str,
+    "year": int,
+    "hour": str,
+    "min": str,
+    "sec": str,
+    "tod": str 
+}
+
+class Units:
+   day: str
+   month: str
+   mday: str
+   year: int
+   hour: str
+   minute: str
+   second: str
+   tod: str
